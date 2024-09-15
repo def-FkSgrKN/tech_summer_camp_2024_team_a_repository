6aa0814f220cb20263247b72a81488136458ddf3
def-FkSgrKN <sogofurukawafs.ks2020.jp@gmail.com>
2024-09-16 05:19:08 +0900

    jsonfyや開始時刻, 終了時刻, ユーザーとの対応を強化しました

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import json
+from pickle import EMPTY_TUPLE
 import uuid
 import os
 import time
@@ -45,8 +47,9 @@ print(User_ID_Table.__tablename__) #実際の表の名前:user_id__table
 
 # UserIDに結びついた開始時刻と終了時刻の全userの一覧データ
 class User_Posture_Time_Record_Table(db.Model):
+    #user_idとposture_detect_start_timeとの復号主キーとする
     user_id = db.Column(db.String(36), db.ForeignKey('user_id__table.user_id', ondelete='CASCADE', onupdate='CASCADE'), primary_key=True)
-    posture_detect_start_time = db.Column(db.Float, nullable=False)
+    posture_detect_start_time = db.Column(db.Float, primary_key=True)
     posture_detect_end_time = db.Column(db.Float, nullable=False)
 
 print(User_Posture_Time_Record_Table.__tablename__) #実際の表の名前:user__posture__time__record__table
@@ -67,15 +70,34 @@ print(User_Posture_Time_Record_Table.__tablename__) #実際の表の名前:user_
 global_user_id = "User_ID_Sample"
 INIT_HUNCHBACK_JUDGEMENT_ALLOWABLE_NUM = 3 #猫背は初期時に3回の残基が与えられる
 
+"""
+クライアントごとにsessionが存在する
+ため, そのUserのsessionが初期化される
+"""
+def initialize_session(user_id):
+    # セッションの初期化
+    session['hunchback_judgement_allowable_num'] = 3  # 許容回数を初期化
+    session['posture_detect_start_time'] = 0       # 開始時刻を初期化
+    session['posture_detect_end_time'] = 0         # 終了時刻を初期化
+    session['user_id'] = user_id          # user_idで誰のsessionかを判断するために重要 ユーザーIDをサンプルで初期化
+
+
+"""
 hunchback_judgement_allowable_num = {} #Userに紐づいた残りの猫背許容回数の辞書
 posture_detect_start_time_global_dict = {} #Userに紐づいた開始時刻の辞書
 posture_detect_end_time_global_dict = {} #Userに紐づいた終了時刻の辞書
+"""
+
+
 
 # データベースを作成
 with app.app_context():
     db.create_all()
 
 # ユーザーを識別するためにUUIDをセッションに保存
+"""
+サンプル例
+
 @app.before_request
 def ensure_user_id():
     if 'user_id' not in session:
@@ -95,6 +117,7 @@ def save_record():
         return jsonify({"message": "Record saved"}), 201
     return jsonify({"error": "No data provided"}), 400
 
+
 # ユーザーの記録を取得するエンドポイント
 @app.route('/records', methods=['GET'])
 def get_records():
@@ -103,10 +126,23 @@ def get_records():
     
     return jsonify([{"id": record.id, "data": record.data} for record in records]), 200
 
+"""
+
+"""
+セッションの初期化
+"""
 # メインページのルート(1頁目)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    page_name = "index"
+
+    
+    #そのユーザのセッションを初期化
+    user_id_from_token = global_user_id
+    initialize_session(user_id_from_token)
+
+    #return render_template('index.html')
+    return jsonify({"page_name":page_name})
 
 
 # 姿勢矯正ページ (2頁目)
@@ -123,18 +159,27 @@ def posture_correction(user_id):
 def posture_correction():
 
     """
-    data = request.get_json()
-    user_id = data.get('user_id') #フロントエンドと合体させるとき
+    session check
     """
-    user_id = global_user_id #デバッグ時
+    user_id_from_token = global_user_id
 
-    #そのユーザのキャプチャ開始時刻(猫背になった時刻ではない)
-    posture_detect_start_time_global_dict[user_id] = time.time()
-    hunchback_judgement_allowable_num[user_id] = INIT_HUNCHBACK_JUDGEMENT_ALLOWABLE_NUM
-    
-    print("posture_detect_start_time_global_dict[user_id]=" + str(posture_detect_start_time_global_dict[user_id]))
+    """
+    開始フラグとして, 開始時刻を0でない現在時刻にする
+    """
+    user_id_from_token = global_user_id
+    user_id_from_session = session.get("user_id")
+   
+    #一致していない場合 おかしいのでerror
+    if user_id_from_token != user_id_from_session:
+        session.clear()  # セッションをクリアする
+        return jsonify({"status_message": "Unauthorized access, please log in again"}), 403
 
-    return render_template('posture_correction.html')
+    session['posture_detect_start_time'] = time.time()
+
+    page_name = "posture_correction"
+    return jsonify({"page_name":page_name}), 200
+    #return render_template('posture_correction.html')
+
 
 """
 ユーザの表への操作
@@ -154,19 +199,34 @@ def search_past_all_user_data():
     data = request.get_json()
     user_id = data.get('user_id') #フロントエンドと合体させるとき
     """
-    user_id = global_user_id #デバッグ時
-
-    past_all_user_data = User_Posture_Time_Record_Table.query.filter_by(id=user_id).all()
     
-    if past_all_user_data:
+    user_id_from_token = global_user_id
+    user_id_from_session = session.get("user_id")
+   
+    #一致していない場合 おかしいのでerror
+    if user_id_from_token != user_id_from_session:
+        session.clear()  # セッションをクリアする
+        return jsonify({"status_message": "Unauthorized access, please log in again"}), 403
+
+    
+
+    past_all_specified_user_data_list = User_Posture_Time_Record_Table.query.filter_by(user_id=user_id_from_token).all()
+    #print("past_all_user_data=")
+    #print(past_all_user_data)
+
+    
+    if past_all_specified_user_data_list:
         # データが見つかった場合
+        posture_detect_start_time_list =  [past_all_specified_user_data.posture_detect_start_time for past_all_specified_user_data in past_all_specified_user_data_list]
+        posture_detect_end_time_list =  [past_all_specified_user_data.posture_detect_end_time for past_all_specified_user_data in past_all_specified_user_data_list]
+
         return jsonify({
-            'posture_detect_start_time': past_all_user_data.posture_detect_start_time,
-            'posture_detect_end_time': past_all_user_data.posture_detect_end_time
-        })
+            'posture_detect_start_time_list': posture_detect_start_time_list,
+            'posture_detect_end_time_list': posture_detect_end_time_list
+        }), 200
     else:
         # データが見つからない場合
-        return 'User not found', 404
+        return jsonify({"status_message":"User not found"}), 404
 
 """
 バックエンド側で, 
@@ -178,8 +238,8 @@ def save_now_user_data(user_id):
     #if data:
     # ユーザーの記録をデータベースに保存
     new_record = User_Posture_Time_Record_Table(user_id=user_id, 
-                                                posture_detect_start_time=posture_detect_start_time_global_dict[user_id], 
-                                                posture_detect_end_time=posture_detect_end_time_global_dict[user_id]
+                                                posture_detect_start_time=session['posture_detect_start_time'],
+                                                posture_detect_end_time=session['posture_detect_end_time']
                                                 )
     db.session.add(new_record)
     db.session.commit()
@@ -197,6 +257,13 @@ https://qiita.com/h_000/items/e5ff1152aae8c67af476
 @app.route('/capture_img<string:user_id>', methods=['POST'])
 def capture_img(user_id):
 """
+
+# 静的ファイル（index.htmlなど）を提供するためのルート
+#試験用(実装外)
+@app.route('/capture_img_front_debug')
+def capture_img_front_debug():
+    return app.send_static_file('capture_img_front_debug.html')  # staticフォルダからindex.htmlを返す
+
 #デバッグ
 @app.route('/capture_img', methods=['POST'])
 def capture_img():
@@ -218,7 +285,15 @@ def capture_img():
     #print(base64_img)
     #print("\n")
 
-    user_id = global_user_id
+    """
+    session check
+    """
+    user_id_from_token = global_user_id
+    user_id_from_session = session.get("user_id")
+    #一致していない場合 おかしいのでerror
+    if user_id_from_token != user_id_from_session:
+        session.clear()  # セッションをクリアする
+        return jsonify({"status_message": "Unauthorized access, please log in again"}), 403
 
 
     results, bad_posture_point, bad_posture_msg_list = img_process_service_instance.detect_hunchback_from_img(base64_img)
@@ -230,7 +305,7 @@ def capture_img():
 
     if bad_posture_point > 0:
         hunchback_judgement = True
-        hunchback_judgement_allowable_num[user_id] -= 1 #猫背の回数だけ減少する
+        session['hunchback_judgement_allowable_num'] -= 1 #猫背の回数だけ減少する
 
 
     print("results=" + str(results))
@@ -243,39 +318,70 @@ def capture_img():
     姿勢推定キャプチャ終了フラグ
     残基が0になったら終了
     """
-    if hunchback_judgement_allowable_num[user_id] == 0:
-        posture_detect_end_time_global_dict[user_id] = time.time()
-        print("posture_detect_end_time_global_dict[user_id]=" + str(posture_detect_end_time_global_dict[user_id]))
+    
+    if session['hunchback_judgement_allowable_num'] == 0:
+        session['posture_detect_end_time'] = time.time()
+        print("session['posture_detect_end_time']=" + str(session['posture_detect_end_time']))
 
-        
-        print("hunchback_judgement_allowable_num=" + str(hunchback_judgement_allowable_num))
-        print("posture_detect_start_time_global_dict=" + str(posture_detect_start_time_global_dict))
-        print("posture_detect_end_time_global_dict=" + str(posture_detect_end_time_global_dict))
+        print("session['hunchback_judgement_allowable_num']=" + str(session['hunchback_judgement_allowable_num']))
+        print("session['posture_detect_start_time']=" + str(session['posture_detect_start_time']))
+        print("session['posture_detect_end_time']=" + str(session['posture_detect_end_time']))
 
         """
         リターン後, 開始時のページに飛べた方が良い
         """
 
+        """
+        終了フラグのときにDBに保存
+        """
+        save_now_user_data(user_id_from_token)
+
+        
+
         # JSONデータに基づいて処理を行う
         response_data = {
             "end_flag":True, 
-            "hunchback_judgement_allowable_num":hunchback_judgement_allowable_num[user_id], #残基
-            "posture_detect_start_time_global_dict": posture_detect_start_time_global_dict[user_id],
-            "posture_detect_end_time_global_dict": posture_detect_end_time_global_dict[user_id],
+            "hunchback_judgement_allowable_num":session['hunchback_judgement_allowable_num'], #残基
+            "posture_detect_start_time": session['posture_detect_start_time'],
+            "posture_detect_end_time": session['posture_detect_end_time'],
+            "hunchback_judgement": hunchback_judgement,
+            "hunchback_message":bad_posture_msg_list
+        }
+
+        """
+        猫背計測後の時間が更新されないように -1を与え, ゲーム終了の印とする
+        """
+        session['hunchback_judgement_allowable_num'] = -1
+
+
+    elif session['hunchback_judgement_allowable_num'] == -1:
+        """
+        姿勢耐久が終了かつデータベースに追加されたとき
+        """
+
+        response_data = {
+            "end_flag":True, 
+            "hunchback_judgement_allowable_num":session['hunchback_judgement_allowable_num'], #残基
+            "posture_detect_start_time_global_dict": session['posture_detect_start_time'],
+            "posture_detect_end_time_global_dict": session['posture_detect_end_time'],
             "hunchback_judgement": hunchback_judgement,
             "hunchback_message":bad_posture_msg_list
         }
 
 
 
+
     else:
+        """
+        姿勢耐久が継続しているとき
+        """
    
         # JSONデータに基づいて処理を行う
         response_data = {
             "end_flag":False, #終了フラグ
-            "hunchback_judgement_allowable_num":hunchback_judgement_allowable_num[user_id], #残基
-            "posture_detect_start_time_global_dict": 0, #開始時刻
-            "posture_detect_end_time_global_dict": 0, #終了時刻
+            "hunchback_judgement_allowable_num":session['hunchback_judgement_allowable_num'], #残基
+            "posture_detect_start_time_global_dict": session['posture_detect_start_time'], #開始時刻
+            "posture_detect_end_time_global_dict": session['posture_detect_end_time'], #終了時刻
             "hunchback_judgement": hunchback_judgement, #猫背判定
             "hunchback_message":bad_posture_msg_list #猫背の説明
         }
@@ -283,7 +389,7 @@ def capture_img():
         print(response_data)
 
         
-        return jsonify(response_data)
+    return jsonify(response_data), 200
 
 
 """
@@ -296,29 +402,49 @@ def capture_img():
 @app.route('/capture_img_end')
 def capture_img_end():
 
-    user_id = global_user_id
-
-    """
-    終了フラグのときにDBに保存
-    """
-    save_now_user_data(user_id)
+    page_name = "capture_img_end"
 
     """
     終了し, DBに追加し終えたUserの開始終了時刻, 猫背残基の辞書からキーを削除
     """
-    hunchback_judgement_allowable_num.pop(user_id)
-    posture_detect_start_time_global_dict.pop(user_id)
-    posture_detect_end_time_global_dict.pop(user_id)
+    #認証されたuser_id
+    user_id_from_token = global_user_id
 
-    return redirect("/") 
+    #sessionのuser_id
+    user_id_from_session = session.get("user_id")
+
+    #一致していない場合 おかしいのでerror
+    if user_id_from_token != user_id_from_session:
+        session.clear()  # セッションをクリアする
+        return jsonify({"status_message": "Unauthorized access, please log in again"}), 403
+
+    #一致している場合に削除する
+    else:
+        session.clear()  # セッションをクリアする
+        return jsonify({"page_name":page_name}), 200
+
+
+    #page_name = "capture_img_end"
+    #return jsonify({"page_name":page_name})
+
 
 
 """
-DB確認用
+DB確認用 実装外
 """
 # DBの内容を表示するルート
 @app.route('/view_hunchback_users_db')
 def view_users():
+    """
+    session check
+    """
+    user_id_from_token = global_user_id
+    user_id_from_session = session.get("user_id")
+    #一致していない場合 おかしいのでerror
+    if user_id_from_token != user_id_from_session:
+        session.clear()  # セッションをクリアする
+        return jsonify({"status_message": "Unauthorized access, please log in again"}), 403
+
     # 全てのユーザーを取得
     users = User_Posture_Time_Record_Table.query.all()
 
